a66ccdaaa30c511eb0dc2c8d3c203e7621407867
Ciorba Antonio <antoniociorba@outlook.com>
2025-10-20 23:18:00 +0200

    created test interfaces, which don't need manual input

---
+++ code/tests/interface/interface_test.h
@@ -0,0 +1,13 @@
+#ifndef OCEANDEPTHS_INTERFACE_TEST_H
+#define OCEANDEPTHS_INTERFACE_TEST_H
+
+#include "interface/interface_table.h"
+
+/**
+ * @brief Test interface that doesn't wait for user input
+ * Used for automated testing
+ */
+extern InterfaceVTable test_interface;
+
+#endif // OCEANDEPTHS_INTERFACE_TEST_H
+
