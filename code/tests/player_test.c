2e01f302de923daedca651e4571e2ae313ba615a
--global <antoniociorba@outlook.com>
2025-10-25 17:57:05 +0200

    changed the test files to the new struct types for each : - player - effect - creature

--- code/tests/player_test.c
+++ code/tests/player_test.c
@@ -28,28 +28,28 @@ int main(void) {
     // Player created with entity base
     printf("CREATED PLAYER: %s\n", p->base.name);
     printf("HP: %d/%d | ATK: %d | DEF: %d | O2: %d/%d | Pearls: %d | Fatigue: %d\n\n",
-           p->base.current_health_points, stat_get_value(&p->base.max_health_points),
+           p->base.current_health_points, p->base.max_health_points,
            stat_get_value(&p->base.attack), stat_get_value(&p->base.defense),
-           p->base.oxygen_level, stat_get_value(&p->base.max_oxygen_level),
+           p->base.oxygen_level, p->base.max_oxygen_level,
            p->pearls, p->base.fatigue_level);
 
     // --- TEST HP ---
     printf("Appliquer 30 de dégâts : \n");
     entity_take_damage(&p->base, 30);
-    printf("HP: %d/%d\n", p->base.current_health_points, stat_get_value(&p->base.max_health_points));
+    printf("HP: %d/%d\n", p->base.current_health_points, p->base.max_health_points);
 
     printf("[HP] Soigne 20 HP : \n");
     entity_recover_hp(&p->base, 20);
-    printf("HP: %d/%d\n\n", p->base.current_health_points, stat_get_value(&p->base.max_health_points));
+    printf("HP: %d/%d\n\n", p->base.current_health_points, p->base.max_health_points);
 
     // --- TEST OXYGEN ---
     printf("[O2] Consomme 30 oxygène :\n");
     consume_oxygen(p, 30);
-    printf("O2: %d/%d\n", p->base.oxygen_level, stat_get_value(&p->base.max_oxygen_level));
+    printf("O2: %d/%d\n", p->base.oxygen_level, p->base.max_oxygen_level);
 
     printf("[O2] Récupère 50 oxygène :\n");
     recover_oxygen(p, 50);
-    printf("O2: %d/%d\n\n", p->base.oxygen_level, stat_get_value(&p->base.max_oxygen_level));
+    printf("O2: %d/%d\n\n", p->base.oxygen_level, p->base.max_oxygen_level);
 
     // --- TEST FATIGUE ---
     printf("[Fatigue] Ajoute 3 fatigue :\n");
