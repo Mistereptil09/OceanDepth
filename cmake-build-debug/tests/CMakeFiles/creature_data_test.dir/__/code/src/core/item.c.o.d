94c65ae52d52f724634686968cb9bf64173148cd
Ciorba Antonio <antoniociorba@outlook.com>
2025-10-28 20:28:17 +0100

    added .gitignore file and removed the debug build of the app from the repo

--- cmake-build-debug/tests/CMakeFiles/creature_data_test.dir/__/code/src/core/item.c.o.d
+++
@@ -1,5 +0,0 @@
-tests/CMakeFiles/creature_data_test.dir/__/code/src/core/item.c.o: \
-  /Users/yasmineslamani/CLionProjects/OceanDepth/code/src/core/item.c \
-  /Users/yasmineslamani/CLionProjects/OceanDepth/code/src/core/../../includes/core/item.h \
-  /Users/yasmineslamani/CLionProjects/OceanDepth/code/src/core/../../includes/core/action.h \
-  /Users/yasmineslamani/CLionProjects/OceanDepth/code/src/core/../../includes/core/effect.h
